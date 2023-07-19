aeb52b3c85f88695a51e998f2b5f229085ca9f05
giang <vuthithaogiang050400@gmail.com>
2023-07-19 20:36:46 +0700

    add

--- src/main/java/com/giang/security/demo/DemoController.java
+++ src/main/java/com/giang/security/demo/DemoController.java
@@ -1,17 +1,41 @@
 package com.giang.security.demo;
 
 
+import com.giang.security.file_storage.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/demo-controller")
 public class DemoController {
 
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
     @GetMapping
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Hello!");
+    }
+
+    @PostMapping("/image")
+    public ResponseEntity<?> uploadImage(@RequestParam("image")MultipartFile file) throws IOException {
+        String uploadImage = fileStorageService.uploadImage(file);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(uploadImage);
+    }
+
+    @GetMapping("/image/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName) {
+        byte[] imageDate = fileStorageService.downloadImage(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageDate);
     }
 }
